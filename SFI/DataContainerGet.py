6edc1c5daafa396d2420ce0327dc871f81eb842e
최송아 <csa912@mobigen.com>
2014-05-27 11:19:58 +0900

    Create DataContainerGet.py

---
+++ SFI/DataContainerGet.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import Mobigen.DataProcessing.DataContainer3 as DataContainer
+
+dc = DataContainer.DataContainer(".", Mode="r")
+ftime, mtime, opt, idx, data = dc.getLast()
+#ftime, mtime, opt, idx, data = dc.get("20120407120000", 100)
+
+while (True) :
+	ftime, mtime, opt, idx, data = dc.next()
+	print data
+
+dc.close()
