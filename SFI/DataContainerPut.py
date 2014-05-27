af476e524381fb70e51190d381cedd21e4342f12
최송아 <csa912@mobigen.com>
2014-05-27 12:20:00 +0900

    Delete DataContainerPut.py

--- SFI/DataContainerPut.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-import Mobigen.DataProcessing.DataContainer3 as DataContainer
-
-dc = DataContainer.DataContainer(".", Mode="a", KeepHour=4, Version=3)
-for i in range(0, 1000) :
-	dc.put("20120426150000", "test %10d" % i)
-dc.close()
